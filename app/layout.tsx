8bc7906bf5964ea7d7ef3fcc0dcc0ca5b8adf714
dobzacode <corentin.kittel@gmail.com>
2024-02-05 10:06:49 +0100

    meta data done

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,18 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Menpo',
   description: 'Techno music producer from France',
+  metadataBase: new URL('https://menpo.fr'),
+  openGraph: {
+    title: 'Menpo',
+    description: 'Techno music producer from France',
+    images: 'url/opengraph-image.jpg'
+  },
+  twitter: {
+
+    title: 'Menpo',
+    description: 'Techno music producer from France',
+    images: 'url/twitter-image.jpg'
+  }
 }
 
 export default function RootLayout({
